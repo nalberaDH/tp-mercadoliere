670264f684da6c64757e0f5513a9ab2ae8b1e9cb
nalberaDH <105679268+nalberaDH@users.noreply.github.com>
2022-06-20 21:45:30 -0300

    actualizo app

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+app.set('puerto',process.env.PORT || 3000)
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
 
-
+app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
